15298269dd6c05fedf985062cec0864c600167b9
John Turner <johnnytee@gmail.com>
2012-05-16 15:44:44 -0400

    Get settings

--- inc/helper-functions.php
+++ inc/helper-functions.php
@@ -5,14 +5,8 @@
 
 function _wpseed_get_options(){
 	$settings = array();
-	var_dump($_wpseed);
-	foreach ($_wpseed->options as $v) {
-		switch ($v['type']) {
-			case 'setting':
-				var_dump($v['id']);
-				$settings = $settings + get_option($v['id']);
-				break;
-		}
-	}
+
+	$settings = get_option('wpseed_settings_1') + get_option('wpseed_settings_2');
+
 	return $settings;
 }
